b1b40dfc1063703c9bacf4174dbd84ea561b520b
Vikram Jadhav <vikramcse.10@gmail.com>
2017-11-16 19:19:46 +0530

    simple button change

--- react-github-repo-viewer/app.js
+++ react-github-repo-viewer/app.js
@@ -1,7 +1,24 @@
-var HelloComponent = React.createClass({
+var Main = React.createClass({
+    getInitialState: function() {
+        return {
+            text: ""
+        }
+    },
+    handleChange: function() {
+        var username = this.refs.search.value;
+        this.setState({
+            text: username
+        });
+    },
     render: function() {
-        return <h1>Hello Visible Alpha!</h1>;
+        return (
+            <div>
+                Enter username <input type="text" ref="search"/>
+                <button onClick={this.handleChange} >Search</button>
+                <div ref="displayArea">My name is {this.state.text}</div>
+            </div>
+        );
     }
 });
 
-ReactDOM.render(<HelloComponent />, document.getElementById('mount'));
+ReactDOM.render(<Main />, document.getElementById('mount'));
