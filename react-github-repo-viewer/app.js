88b58ad5fb747732c12177fa07806197823cc836
Vikram Jadhav <vikramcse.10@gmail.com>
2017-11-16 19:47:22 +0530

    separate button component and props demo

--- react-github-repo-viewer/app.js
+++ react-github-repo-viewer/app.js
@@ -1,3 +1,13 @@
+var Button = React.createClass({
+    handleClick: function(e) {
+        e.preventDefault();
+        this.props.handleButtonClick();
+    },
+    render: function() {
+        return <button onClick={this.handleClick} ref="search">{this.props.buttonText}</button>
+    }
+});
+
 var Main = React.createClass({
     getInitialState: function() {
         return {
@@ -13,8 +23,8 @@ var Main = React.createClass({
     render: function() {
         return (
             <div>
-                Enter username <input type="text" ref="search"/>
-                <button onClick={this.handleChange} >Search</button>
+                Enter username <input type="text" ref="search" placeholder="Enter username"/> &nbsp;
+                <Button handleButtonClick={this.handleChange} buttonText="search username"/>
                 <div ref="displayArea">My name is {this.state.text}</div>
             </div>
         );
