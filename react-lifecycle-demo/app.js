dd6f9e53f30982db604d148b248ccbd6b4fdcbfa
Vikram Jadhav <vikramcse.10@gmail.com>
2017-11-19 10:59:30 +0530

    added default props and console statements

--- react-lifecycle-demo/app.js
+++ react-lifecycle-demo/app.js
@@ -1,5 +1,15 @@
 var Counter = React.createClass({
+    getDefaultProps: function() {
+        console.log('getDefaultProps called');
+
+        return {
+            title: 'A Counter Example'
+        }
+    },
+
     getInitialState: function() {
+        console.log("getInitialState called");
+
         return {
             count: 0
         }
@@ -9,6 +19,7 @@ var Counter = React.createClass({
         console.log('rendering stuff');
         return (
             <div>
+                <h1>{this.props.title}</h1>
                 <div>{this.state.count}</div>
                 <input type='button' value='+' />
                 <input type='button' value='-' />
