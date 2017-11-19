289e73fddccd46d8462373f9dcc7e59b09202153
Vikram Jadhav <vikramcse.10@gmail.com>
2017-11-19 11:14:43 +0530

    added handle events

--- react-lifecycle-demo/app.js
+++ react-lifecycle-demo/app.js
@@ -18,14 +18,28 @@ var Counter = React.createClass({
         }
     },
 
+    handleIncrement: function() {
+        var incCount = this.state.count + 1;
+        this.setState({
+            count: incCount
+        });
+    },
+
+    handleDecrement: function() {
+        var decCount = this.state.count - 1;
+        this.setState({
+            count: decCount
+        });
+    },
+
     render: function() {
         console.log('rendering stuff');
         return (
             <div>
                 <h1>{this.props.title}</h1>
                 <div>{this.state.count}</div>
-                <input type='button' value='+' />
-                <input type='button' value='-' />
+                <input type='button' value='+' onClick={this.handleIncrement}/>
+                <input type='button' value='-' onClick={this.handleDecrement}/>
             </div>
         );
     }
